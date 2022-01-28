bf985b647b414536de005093022341b7908a88a2
EvgeniyTTI <evgeniytti@rambler.ru>
2022-01-28 03:18:35 +0300

    and TypeScript(withOUT db object)

--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,6 @@
 'use strict';
 
-import { Model } from 'sequelize';
+import { Model, } from 'sequelize';
 
 export default (sequelize, DataTypes) => {
   class User extends Model {
@@ -9,9 +9,9 @@ export default (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    /* static associate(models) {
       // define association here
-    }
+    } */
   }
   User.init({
     id: {
